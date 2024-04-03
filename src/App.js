bff313a9472ae98ed3daf201ab152842791664e5
hemantvardani <hemant2vardani@gmail.com>
2024-04-03 20:52:19 +0530

    also added delete and edit options to form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React ,{useEffect, useState} from 'react';
 import axios from 'axios';
 import 'semantic-ui-css/semantic.min.css'
-import { Table, Button } from 'semantic-ui-react'
+import { Table, Button , Input } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 
 
@@ -10,7 +10,7 @@ import 'semantic-ui-css/semantic.min.css';
 import loadingGif from './icons8-loading-circle.gif'
 
 
-const REACT_APP_GOOGLE_SHEET_LINK = "https://sheet.best/api/sheets/3c5fd47a-1f63-4e5b-89cd-d9e7ea8d809a"
+const REACT_APP_GOOGLE_SHEET_LINK = "https://sheet.best/api/sheets/fbfa669c-2fdd-4bfb-bc42-9bfe7fe2093c"
 
 
 
@@ -23,6 +23,7 @@ function App() {
 const [apiData,setApiData] = useState([]);
 const [refreshIcon, setRefreshIcon]= useState(false);
 const [showSubmittingMessage, setShowSubmittingMessage] = useState(false);
+const [editableData, setEditableData]= useState(null);
 
 
   function submitFunction(e){
@@ -60,6 +61,7 @@ const [showSubmittingMessage, setShowSubmittingMessage] = useState(false);
   useEffect(()=>{ fetchData();  },[refreshIcon]);
 
 
+
   function scrollToSection2() {
                   const section2 = document.getElementById('section2');
                   if (section2) {
@@ -82,6 +84,38 @@ const [showSubmittingMessage, setShowSubmittingMessage] = useState(false);
     SyncButtonFun();
   }
 
+  function deleteRow(index)
+  {
+    console.log(index)
+    console.log(apiData.length)
+    const newApiData=[...apiData];
+    newApiData.splice(index,1);
+    console.log(newApiData.length)
+    setApiData(newApiData)
+    axios.delete(`${REACT_APP_GOOGLE_SHEET_LINK}/${index}`).then((e)=>{console.log(e.data)});
+  }
+
+  function editRow(index){
+    setEditableData({index:index,
+      ID:apiData[index].ID,
+      "Avatar_Name":apiData[index].Avatar_Name,
+       "Performance_Score":apiData[index].Performance_Score
+    });
+  }
+
+  function submitEditedRow(index){
+    const dupApiData=[...apiData];
+    dupApiData[index]={ ID:editableData.ID,
+      "Avatar_Name":editableData.Avatar_Name,
+       "Performance_Score":editableData.Performance_Score};
+
+    setApiData(dupApiData)
+    console.log("he"+ JSON.stringify(dupApiData[index],null,2));
+    axios.put(`${REACT_APP_GOOGLE_SHEET_LINK}/${index}`,dupApiData[index]);
+    setEditableData(null)
+
+  }
+           
 
 
 
@@ -224,34 +258,94 @@ const [showSubmittingMessage, setShowSubmittingMessage] = useState(false);
                                             <Table.HeaderCell>ID</Table.HeaderCell>
                                             <Table.HeaderCell>Avatar Name</Table.HeaderCell>
                                             <Table.HeaderCell>Performance Score</Table.HeaderCell>
+                                            <Table.HeaderCell></Table.HeaderCell>
                                             
                                           </Table.Row>
                                         </Table.Header>
 
                                     <Table.Body>
                                     
-                                    {  
-                                    apiData.map((data)=>{
+                                    {
+                                    
+                                    
+                                    apiData.map((data,index)=>{
 
                                       return (
 
+                                    editableData?.index===index
+                                      ?
+                                       
+                                       <Table.Row  >
+                                         <Table.Cell>
+                                           <Input value={editableData.ID} onChange={(e)=>{setEditableData({...editableData,ID:e.target.value})}}>
+                                           </Input>
+ 
+                                         </Table.Cell>
+ 
+                                         <Table.Cell>
+                                           <Input value={editableData.Avatar_Name} onChange={(e)=>{  setEditableData({...editableData,Avatar_Name:e.target.value})}}>
+                                           </Input>
+ 
+                                         </Table.Cell>
+ 
+ 
+                                         <Table.Cell>
+                                           <Input value={editableData.Performance_Score} onChange={(e)=>{   setEditableData({...editableData,Performance_Score:e.target.value})}}>
+                                           </Input>
+ 
+                                         </Table.Cell>
+                                         <Table.Cell>
+                                         <i class="check circle icon" style={{color:'#179c03' }}  onClick={()=>{submitEditedRow(index)}}></i>
+                                  
+                                         </Table.Cell>
+               
+                                       </Table.Row>
+                                      :
+                                      <Table.Row className= 'row-hover' disabled={editableData?true:false}>
+
                                         
-                                        <Table.Row>
-                                        <Table.Cell>{data.ID}</Table.Cell>
-                                        <Table.Cell>{data.Avatar_Name}</Table.Cell>
-                                        <Table.Cell>{data.Performance_Score}</Table.Cell>
-                                      
-                                      </Table.Row>
+                                      <Table.Cell>{data.ID}                                       
+                                      </Table.Cell>
+                                      <Table.Cell>{data.Avatar_Name}</Table.Cell>
+                                      <Table.Cell>
+                                         
+                                          
+                                          {data.Performance_Score} 
+                                          
+                                          </Table.Cell> 
+                                          <Table.Cell className='editOrDelete' style={{marginLeft:40}}>
+                                           <i class="edit icon"  style={{color:'#91ed72'}} onClick={()=>editRow(index)} ></i>
+                                         <i class="trash alternate icon" style={{color:'#f04d4d'}} onClick={()=>
+                                          deleteRow(index)
+                                         }   ></i>
+                                        
+
+                                       
+                                         
+                                        
+                                      </Table.Cell>              
+                                    </Table.Row>
+                                    )
+
+
+
+                                   
+                                     
+
+
+                                       
+                                        
 
                                         
                                       
-                                      )
+                                      
 
                                     })
                                     }
                                     
                                     
 
+                                    
                                     </Table.Body>
                                         </Table>
                                 </div>
